ccd7ad3234931fc80688205884d20a97e6b4b289
micah reich <micahreich02@gmail.com>
2023-12-07 04:14:18 -0500

    fmt

--- openmp/src/main.cpp
+++ openmp/src/main.cpp
@@ -1,26 +1,27 @@
 #include <stdint.h>
+
 #include <iostream>
-#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
 #define PATCH_SIZE 7
 #define HALF_SIZE PATCH_SIZE / 2
 #define BRUSH_SIZE_MULT 0.2f
 
-#define TOGGLE_FILL_ON '1'        // ASCII for 1
-#define TOGGLE_ERASE_ON '2'       // ASCII for 2
-#define TOGGLE_BRUSH_RAD_INCR 'x' // ASCII for x
-#define TOGGLE_BRUSH_RAD_DECR 'z' // ASCII for z
+#define TOGGLE_FILL_ON '1'         // ASCII for 1
+#define TOGGLE_ERASE_ON '2'        // ASCII for 2
+#define TOGGLE_BRUSH_RAD_INCR 'x'  // ASCII for x
+#define TOGGLE_BRUSH_RAD_DECR 'z'  // ASCII for z
 #define TOGGLE_SAVE 's'
 
 const unsigned char MASK_COLOR[] = {0, 255, 0};
-const unsigned char COLOR_WHITE[] = {255,255,255};
-const unsigned char COLOR_BLACK[] = {0,0,0};
+const unsigned char COLOR_WHITE[] = {255, 255, 255};
+const unsigned char COLOR_BLACK[] = {0, 0, 0};
 
+#include <unistd.h>
 
 #include "patch_match.h"
 #include "patch_match_utils.h"
-#include <unistd.h>
 
 using namespace std;
 
@@ -31,12 +32,11 @@ enum FillMode { ERASE = 0, FILL = 1 };
 
 cv::Point lastPoint(-1, -1);
 cv::Point currentMousePos(-1, -1);
-int brushRadius = 15; // Initial brush radius
+int brushRadius = 15;  // Initial brush radius
 FillMode curr_mode = FillMode::FILL;
 cv::Mat drawingLayer;
 int n_levels = 0, patch_size = 0, lambda = 0;
 int minimum_levels = 1, minimum_patch_size = 5, minimum_lambda = 1;
-
 
 void onMouse(int event, int x, int y, int flags, void* userdata) {
     currentMousePos = {x, y};
@@ -44,8 +44,11 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
         lastPoint = cv::Point(x, y);
     } else if (event == cv::EVENT_MOUSEMOVE && (flags & cv::EVENT_FLAG_LBUTTON)) {
         if (lastPoint.x != -1) {
-            cv::Scalar color = curr_mode == FillMode::ERASE ? cv::Scalar(0, 0, 0, 0) : cv::Scalar(57, 255, 20, 1); // Transparent for eraser, red for drawing
-            // int thickness = curr_mode == FillMode::ERASE ? brushRadius * 2 : brushRadius; // Larger thickness for eraser for easier erasing
+            cv::Scalar color = curr_mode == FillMode::ERASE
+                                   ? cv::Scalar(0, 0, 0, 0)
+                                   : cv::Scalar(57, 255, 20, 1);  // Transparent for eraser, red for drawing
+            // int thickness = curr_mode == FillMode::ERASE ? brushRadius * 2 : brushRadius; // Larger thickness for
+            // eraser for easier erasing
             cv::line(drawingLayer, lastPoint, cv::Point(x, y), color, brushRadius, cv::LINE_AA, 0);
             lastPoint = cv::Point(x, y);
         }
@@ -54,21 +57,15 @@ void onMouse(int event, int x, int y, int flags, void* userdata) {
     }
 }
 
-void onLevelsChange(int new_value, void* userdata) {
-    n_levels = minimum_levels+new_value;
-}
+void onLevelsChange(int new_value, void* userdata) { n_levels = minimum_levels + new_value; }
 
-void onPatchSizeChange(int new_value, void* userdata) {
-    patch_size = minimum_patch_size+new_value;
-}
+void onPatchSizeChange(int new_value, void* userdata) { patch_size = minimum_patch_size + new_value; }
 
-void onLambdaChange(int new_value, void* userdata) {
-    lambda = minimum_lambda+new_value;
-}
+void onLambdaChange(int new_value, void* userdata) { lambda = minimum_lambda + new_value; }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     // Read in user image with -i command line flag, or use lena.png as default
-    const char *image_path = "src/lena.png";
+    const char* image_path = "src/lena.png";
     int opt;
     while ((opt = getopt(argc, argv, "di:")) != -1) {
         switch (opt) {
@@ -109,8 +106,9 @@ int main(int argc, char *argv[]) {
     //     cv::cvtColor(image, tempImage, cv::COLOR_BGR2BGRA); // Convert image to have an alpha channel
     //     cv::addWeighted(tempImage, 1.0, drawingLayer, 1.0, 0, displayImage);
     //     if (currentMousePos.x != -1 && currentMousePos.y != -1) {
-    //         cv::Scalar outlineColor = curr_mode == FillMode::ERASE ? cv::Scalar(0, 0, 255, 255) : cv::Scalar(255, 0, 0, 255); // Blue for eraser, red for drawing
-    //         cv::circle(displayImage, currentMousePos, brushRadius/2, outlineColor, 1);
+    //         cv::Scalar outlineColor = curr_mode == FillMode::ERASE ? cv::Scalar(0, 0, 255, 255) : cv::Scalar(255, 0,
+    //         0, 255); // Blue for eraser, red for drawing cv::circle(displayImage, currentMousePos, brushRadius/2,
+    //         outlineColor, 1);
     //     }
     //     int fontFace = cv::FONT_HERSHEY_SIMPLEX;
     //     double fontScale = 0.5;
@@ -119,10 +117,9 @@ int main(int argc, char *argv[]) {
     //     cv::Point offset(0, 20);
     //     cv::Scalar textColor(0, 255, 0); // Green color
 
-    //     cv::putText(displayImage, "Keys: [x]/[z] to change brush size", textOrg, fontFace, fontScale, textColor, thickness);
-    //     cv::putText(displayImage, "Keys: [1]/[2] to change fill mdoe", textOrg+offset, fontFace, fontScale, textColor, thickness);
-        
-        
+    //     cv::putText(displayImage, "Keys: [x]/[z] to change brush size", textOrg, fontFace, fontScale, textColor,
+    //     thickness); cv::putText(displayImage, "Keys: [1]/[2] to change fill mdoe", textOrg+offset, fontFace,
+    //     fontScale, textColor, thickness);
 
     //     cv::imshow(WINDOW_NAME, displayImage);
 
@@ -151,7 +148,6 @@ int main(int argc, char *argv[]) {
     // cv::threshold(mask, mask, 1, 255, cv::THRESH_BINARY);
     // cv::Mat mask_mat = mask > 0;
 
-
     // Prints the mask to visually inspect and ensure its correct
     // for(int i = 0; i < image.rows; i++) {
     //     for(int j = 0; j < image.cols; j++) {
@@ -159,7 +155,7 @@ int main(int argc, char *argv[]) {
     //     }
     //     printf("\n");
     // }
-    
+
     /*PATCH MATCH INIT*/
     // PatchMatchParams params = PatchMatchParams();
     // params.n_levels = n_levels;
@@ -196,7 +192,6 @@ int main(int argc, char *argv[]) {
     // Normalize binary_mask to 0-255
     cv::Mat normalized_mask;
     binary_mask.convertTo(normalized_mask, CV_8UC1, 255);
-    
 
     PatchMatchParams params = PatchMatchParams();
     PatchMatchInpainter inpainter(params, test_image, binary_mask);
